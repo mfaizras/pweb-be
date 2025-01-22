50d3ed5b48a3ddf7bb41b6986f418eecfe13a432
Faiz Rashid <mfaizras@gmail.com>
2025-01-23 02:52:21 +0700

    add Api login, register, etc

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,8 +18,8 @@ class CorsMiddleware
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', env('FRONTEND_URL', 'http://localhost:3000'));
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         return $response;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // ForceJsonMiddleware::class
-            // CorsMiddleware::class
+            CorsMiddleware::class
 
         ]);
 
